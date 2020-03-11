315ee793a90ded3f4554cf2e4857504bd7ee0615
ZeuberlichStudio <trifonov.ya@yandex.ru>
2020-03-11 16:43:04 +0300

    Updates

---
+++ precache-manifest.fe5abeac1e83eed36551a58b02f2bab0.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2001564121cae674d1822a97f2597c07",
+    "url": "/kakvkino/index.html"
+  },
+  {
+    "revision": "e75eae1925b64a7d1918",
+    "url": "/kakvkino/static/css/main.9f6e0d4f.chunk.css"
+  },
+  {
+    "revision": "ee21d2c4e177d8ffa21e",
+    "url": "/kakvkino/static/js/2.ffa80e73.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kakvkino/static/js/2.ffa80e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75eae1925b64a7d1918",
+    "url": "/kakvkino/static/js/main.7eacad29.chunk.js"
+  },
+  {
+    "revision": "901c51991995dbdfcd56",
+    "url": "/kakvkino/static/js/runtime-main.52ee6f4c.js"
+  },
+  {
+    "revision": "cfa6f7f354eca8cb83e14a650341c12f",
+    "url": "/kakvkino/static/media/BebasNeueBold.cfa6f7f3.woff"
+  },
+  {
+    "revision": "a9a80ec3e8894233cca1758e811f619e",
+    "url": "/kakvkino/static/media/HelveticaNeue-Bold.a9a80ec3.woff"
+  },
+  {
+    "revision": "88b7a427cf1aec1984382459d5ca12e0",
+    "url": "/kakvkino/static/media/HelveticaNeue-Regular.88b7a427.woff"
+  },
+  {
+    "revision": "2f0f58be1882e52d73f386025c19e99c",
+    "url": "/kakvkino/static/media/Pattern_1.2f0f58be.svg"
+  },
+  {
+    "revision": "7726e8f888192c181dfc3359019a1159",
+    "url": "/kakvkino/static/media/about_bg_1.7726e8f8.jpg"
+  },
+  {
+    "revision": "8bd22a62935248b0f941193462e62f18",
+    "url": "/kakvkino/static/media/about_bg_2.8bd22a62.jpg"
+  },
+  {
+    "revision": "13e34a012d5bffc6b8e3e25de50c14ac",
+    "url": "/kakvkino/static/media/about_bg_3.13e34a01.jpg"
+  },
+  {
+    "revision": "d12e8a5289f97c048db4e047a9f8f066",
+    "url": "/kakvkino/static/media/arrow_l.d12e8a52.svg"
+  },
+  {
+    "revision": "5e819f15c8c0d971a75dcb8b3621a080",
+    "url": "/kakvkino/static/media/arrow_r.5e819f15.svg"
+  },
+  {
+    "revision": "e2df32a301a75282636acab7cd002d32",
+    "url": "/kakvkino/static/media/close.e2df32a3.svg"
+  },
+  {
+    "revision": "035593b8601040bf12fc9812a83efb52",
+    "url": "/kakvkino/static/media/comparison.035593b8.svg"
+  },
+  {
+    "revision": "bb358cf15b236a7c63e17069eb9795b3",
+    "url": "/kakvkino/static/media/empty_blocks.bb358cf1.svg"
+  },
+  {
+    "revision": "77752253eb7d870e76a0546b52cdc9e5",
+    "url": "/kakvkino/static/media/eurasianLogo.77752253.svg"
+  },
+  {
+    "revision": "8f78fd88095073e449f41ef498e5ca92",
+    "url": "/kakvkino/static/media/fb_dark_icon.8f78fd88.svg"
+  },
+  {
+    "revision": "18b5c45539d9d4c6c408d4e61aa941ae",
+    "url": "/kakvkino/static/media/fb_icon.18b5c455.svg"
+  },
+  {
+    "revision": "bc4ce87163553fea90183ee2ade7d0bc",
+    "url": "/kakvkino/static/media/figure_border.bc4ce871.svg"
+  },
+  {
+    "revision": "be90a0c3c8e6f70bbe1da9ddb3c598e3",
+    "url": "/kakvkino/static/media/figure_border.be90a0c3.svg"
+  },
+  {
+    "revision": "dc2d603a99864e6ff146620daec4f355",
+    "url": "/kakvkino/static/media/glasses.dc2d603a.svg"
+  },
+  {
+    "revision": "920635223a557079d4014490ba005772",
+    "url": "/kakvkino/static/media/inst_dark_icon.92063522.svg"
+  },
+  {
+    "revision": "661b2886611440c409e1312f07844d4a",
+    "url": "/kakvkino/static/media/inst_icon.661b2886.svg"
+  },
+  {
+    "revision": "1b35c3ca69caaa9780dda1c9fc6478da",
+    "url": "/kakvkino/static/media/logo.1b35c3ca.svg"
+  },
+  {
+    "revision": "146a5073261a8775db2b7d4c8d5a2757",
+    "url": "/kakvkino/static/media/map.146a5073.png"
+  },
+  {
+    "revision": "0c3d097cc47cc7a6106997a37daf8fd1",
+    "url": "/kakvkino/static/media/open_menu.0c3d097c.svg"
+  },
+  {
+    "revision": "7c9f4100d80872f3196e3c9d05c2334e",
+    "url": "/kakvkino/static/media/origoLogo.7c9f4100.svg"
+  },
+  {
+    "revision": "9983fdd9702f42ffc8558fa23b6cc458",
+    "url": "/kakvkino/static/media/paragraphLogo.9983fdd9.svg"
+  },
+  {
+    "revision": "d15680431ee81455009d3f9c6482e7ba",
+    "url": "/kakvkino/static/media/planLogo.d1568043.svg"
+  },
+  {
+    "revision": "33ddc90ecd1958fb2305618d92001dce",
+    "url": "/kakvkino/static/media/popcorn.33ddc90e.svg"
+  },
+  {
+    "revision": "ed927c5987bcde64d0b025ed39c51e00",
+    "url": "/kakvkino/static/media/popcorn.ed927c59.svg"
+  },
+  {
+    "revision": "7084084e3b05d68cdded8d62ebeff22c",
+    "url": "/kakvkino/static/media/service_bg.7084084e.jpg"
+  },
+  {
+    "revision": "13151f993ee86a20a7b6a24d8dfc7a32",
+    "url": "/kakvkino/static/media/stats_graph.13151f99.svg"
+  },
+  {
+    "revision": "3581ae34508dbc80872817a18643bd69",
+    "url": "/kakvkino/static/media/stats_graph.3581ae34.svg"
+  },
+  {
+    "revision": "1a0ad887bc7997220c57e11c10e87fd2",
+    "url": "/kakvkino/static/media/stats_pic_1.1a0ad887.svg"
+  },
+  {
+    "revision": "868c37e3050ec46950160db58f8b94e4",
+    "url": "/kakvkino/static/media/stats_pic_2.868c37e3.svg"
+  },
+  {
+    "revision": "642db18009067938aa9901cb5f8fd46b",
+    "url": "/kakvkino/static/media/stats_stages.642db180.svg"
+  },
+  {
+    "revision": "ec87554f3fab5a799af74e850989db07",
+    "url": "/kakvkino/static/media/tabs_1.ec87554f.svg"
+  },
+  {
+    "revision": "17b15824ba234ffe3f1c08f4e6262475",
+    "url": "/kakvkino/static/media/tabs_2.17b15824.svg"
+  },
+  {
+    "revision": "eae3e2bfdda67adf745c1d44be9f32bb",
+    "url": "/kakvkino/static/media/tabs_3.eae3e2bf.svg"
+  },
+  {
+    "revision": "6881d60c6939ad021dd2b18e8511628c",
+    "url": "/kakvkino/static/media/tabs_gradient.6881d60c.svg"
+  },
+  {
+    "revision": "2c098884b8e79e48c88e6d97d778b0ec",
+    "url": "/kakvkino/static/media/timeline.2c098884.svg"
+  },
+  {
+    "revision": "a6085382e386f209bde8149d15fd40ca",
+    "url": "/kakvkino/static/media/title.a6085382.png"
+  },
+  {
+    "revision": "9b5c30acf18be164609b800378837834",
+    "url": "/kakvkino/static/media/title_stats_1.9b5c30ac.svg"
+  },
+  {
+    "revision": "6cdc901b7abc4a43c8141a626d76e866",
+    "url": "/kakvkino/static/media/title_stats_2.6cdc901b.svg"
+  },
+  {
+    "revision": "b3eb5bbed58c463adb954705cf3d8fa2",
+    "url": "/kakvkino/static/media/title_stats_3.b3eb5bbe.svg"
+  },
+  {
+    "revision": "df4ab639f7dbdbb5877d93671965881e",
+    "url": "/kakvkino/static/media/title_stats_4.df4ab639.svg"
+  },
+  {
+    "revision": "a43461fc0823384234dfa06ec69126ee",
+    "url": "/kakvkino/static/media/wa_dark_icon.a43461fc.svg"
+  },
+  {
+    "revision": "91c2df27d621ae7d80d112b67fbb3037",
+    "url": "/kakvkino/static/media/wa_icon.91c2df27.svg"
+  }
+]);
