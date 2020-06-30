85b23714fdbfef88fe431d2cafe03578fd88dce0
Yaroslav <goooglemaaan@gmail.com>
2020-06-30 21:34:01 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kakvkino/precache-manifest.9e325584592d96053441fa55347cdc10.js"
+  "/kakvkino/precache-manifest.13efde3b0499d6b4c0f97feaa0c29f82.js"
 );
 
 self.addEventListener('message', (event) => {
